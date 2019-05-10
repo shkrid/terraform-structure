6969f156a57b43f639f40e3f3fef56e3289532bd
Artem Yasinskiy <shkrid@gmail.com>
2019-05-10 23:42:33 +0300

    add s3 backaned

--- 1terraform/global/rs/state.tf
+++ 1terraform/global/rs/state.tf
@@ -1,9 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "1terraform-remote-state-global"
-#     key            = "global/rs/terraform.tfstate"
-#     region         = "us-east-1"
-#     encrypt        = true
-#     dynamodb_table = "1terraform-remote-state-global"
-#   }
-# }
+# Comment out on firts init
+terraform {
+  backend "s3" {
+    bucket         = "1terraform-remote-state-global"
+    key            = "global/rs/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "1terraform-remote-state-global"
+  }
+}
