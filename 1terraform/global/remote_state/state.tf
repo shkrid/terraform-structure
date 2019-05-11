cc7cebe6c731477b301527a4c54000675b86bf4a
Artem Yasinskiy <shkrid@gmail.com>
2019-05-12 01:51:08 +0300

    fix list

--- 1terraform/global/remote_state/state.tf
+++ 1terraform/global/remote_state/state.tf
@@ -1,11 +1,11 @@
 # Comment out on firts init
-terraform {
-  backend "s3" {
-    bucket  = "1terraform-remote-state-global"
-    key     = "global/rs/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+# terraform {
+#   backend "s3" {
+#     bucket  = "1terraform-remote-state-global"
+#     key     = "global/rs/terraform.tfstate"
+#     region  = "us-east-1"
+#     encrypt = true
 
-    # dynamodb_table = "1terraform-remote-state-global"
-  }
-}
+#     # dynamodb_table = "1terraform-remote-state-global"
+#   }
+# }
